99e2744d6109b34938c3f41725b3ddcb0367af7b
jsdevper <outhkl9716@outlook.com>
2023-02-16 22:13:42 +0900

    20230216

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ForbiddenException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//파이프: 데이터가 지나가는 통로
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -26,22 +27,23 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieid: string): Movie {
+  getOne(@Param('id') movieid: number): Movie {
+    console.log(typeof movieid);
     return this.moviesService.getOne(movieid);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.update(movieId, updateData);
   }
 }
